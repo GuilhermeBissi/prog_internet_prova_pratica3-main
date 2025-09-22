590d41fdbce4607d4277dbfb030aa08d03c3ff2c
GuilhermeBissi <20233005484@estudantes.ifpr.edu.br>
2025-09-22 11:12:03 -0300

    COMITADO

--- server.js
+++ server.js
@@ -9,16 +9,8 @@ const PORT = 1122;
 // Middleware para interpretar JSON
 app.use(express.json());
 
-// Servir arquivos estáticos do frontend (pasta public)
-app.use(express.static(path.join(__dirname, 'public')));
-
 // Rotas da API (CRUD de alunos)
 app.use('/api/alunos', alunosRoutes);
-
-// Rota raiz para servir o index.html
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
 
 // Inicialização do servidor
 app.listen(PORT, () => {
